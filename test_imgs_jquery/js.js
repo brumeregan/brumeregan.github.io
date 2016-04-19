3ce06af3339482cb812fac6b48d69bb5791b0861
Oksana Boyarko <brumeregan@gmail.com>
2016-04-19 17:16:02 +0300

    added file for testing width() method jquery

---
+++ test_imgs_jquery/js.js
@@ -0,0 +1,7 @@
+$(function(){
+	var $firstWidth = $('.first').width();
+	console.log('Размер li, если путь картинки абсолютный', $firstWidth);
+
+	var $secondWidth = $('.second').width();
+	console.log('Размер li, если путь картинки относительный' ,$secondWidth);
+}); 
