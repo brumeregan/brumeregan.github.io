77b318302f2321ae8753906b4fe9d0747a5c35c0
Oksana Boyarko <brumeregan@gmail.com>
2016-04-29 16:16:24 +0300

    delete encodeURI

--- js_15-16/google/js/js.js
+++ js_15-16/google/js/js.js
@@ -24,7 +24,7 @@ $(function(){
 		var keyAPI = "ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg";
 
 		var url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=" +
-							keyAPI + " &rsz=large&start=" + pageIndex + "&q=" + encodeURIComponent(queryText) + "&callback=GoogleCallback&context=?";
+							keyAPI + " &rsz=large&start=" + pageIndex + "&q=" + queryText + "&callback=GoogleCallback&context=?";
    
 		$.ajax({
 			url: url,
