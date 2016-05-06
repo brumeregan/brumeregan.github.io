69503bbeffe0a229981bd394059401c1ae45bd5a
Oksana Boyarko <brumeregan@gmail.com>
2016-05-06 12:46:08 +0300

    js 17-18 - page craeted with grunt

--- js_17-18/Gruntfile.js
+++ js_17-18/Gruntfile.js
@@ -1,19 +1,50 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-	  concat: {
-	    options: {
-	      separator: ';',
+	  concat: {  
+	    css: {
+	    	options: {
+	    		separator: '\n'
+	    	},
+	    	src: ['css/src/*.css'],
+	    	dest: 'css/dist/allstyle.css'
 	    },
-	    dist: {
-	      src: ['js/**/*.js'],
-	      dest: 'js/dist/built.js',
-	    },
-	  }
+	    js: {
+	    	options: {
+	    		separator: ';\n'
+	    	},
+	    	src: ['js/src/*.js'],
+	    	dest: 'js/dist/script.min.js'
+	    }
+	  },
+	 	cssmin: {
+	  	min: {
+	  		src: ['css/dist/allstyle.css'],
+	  		dest: 'css/dist/style.min.css'
+	  	}
+	  },
+	  uglify: {
+	  	dist: {
+	  		src: ['js/dist/script.min.js'],
+		  	dest: 'js/dist/script.min.js'
+		  }
+	  },
+	  watch: {
+  		css: {
+    		files: ['css/src/*.css', 'js/src/*.js'],
+    		tasks: ['concat'],
+    		options: {
+      		spawn: false,
+      		reload: true
+    		},
+  		},
+		},
   });
+
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['concat']);
-
-
+  grunt.registerTask('default', ['concat', 'cssmin', 'uglify']);
 };
