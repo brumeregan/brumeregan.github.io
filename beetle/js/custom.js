d761337b49c384cb2b864f419148a24eee32cb7b
Oksana Boyarko <brumeregan@gmail.com>
2016-08-09 13:35:06 +0300

    renew old files with new without bugs

--- beetle/js/custom.js
+++ beetle/js/custom.js
@@ -29,8 +29,6 @@ $(document).ready(function(){
 	}
 	drawChart();
 
-
-	
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
