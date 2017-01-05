7ed82e5d6d60196a6b55ffbd1449ed9a0273b879
Oksana Boyarko <brumeregan@gmail.com>
2017-01-05 22:09:52 +0200

    react forms state functions

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
 // import Dropdown from './Dropdown';
-import Header from './Header';
-
-const menu = [
-  {
-    link: '/articles',
-    label: 'Artciles'
-  },
-  {
-    link: '/contacts',
-    label: 'Contucts'
-  },
-  {
-    link: '/posts',
-    label: 'Posts'
-  }
-];
+// import Header from './Header';
+import RegistrationForm from './RegistrationForm';
+// const menu = [
+//   {
+//     link: '/articles',
+//     label: 'Artciles'
+//   },
+//   {
+//     link: '/contacts',
+//     label: 'Contucts'
+//   },
+//   {
+//     link: '/posts',
+//     label: 'Posts'
+//   }
+// ];
+// <Header items={menu}/>
 
 class App extends Component {
 
   render() {
     return ( 
       <div>
-        <Header items={menu}/>
+        
+        <RegistrationForm />
       </div>
     );
   }
