b1fd02eba59f397ea3a5fb27f145d1f17bd91eaf
Oksana Boyarko <brumeregan@gmail.com>
2017-01-05 21:50:02 +0200

    added prop validation

--- react/hello-world/src/Header.js
+++ react/hello-world/src/Header.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Header extends Component {
+	
+	static propTypes = {
+		items: PropTypes.array.isRequired
+		// isLoading: PropTypes.bool,
+		// submit: PropTypes.func.isRequired,
+		// title: PropTypes.string.isRequired,
+		// type: PropTypes.oneOf(['news', 'photos']),
+		// user: PropTypes.shape({
+		// 	name: PropTypes.string,
+		// 	age: PropTypes.number
+		// }),
+		// users: PropTypes.arrayOf(
+		// 	PropTypes.shape({
+		// 		name: PropTypes.string,
+		// 		age: PropTypes.number
+		// 	})
+		// )
+	};
+
 
   render() {
-  	console.log('items', this.props.items);
+  	// console.log('items', this.props.items);
     return ( 
     	<div>
       	{this.props.items.map((item, index) =>
